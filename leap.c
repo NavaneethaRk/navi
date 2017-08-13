ecc9c8c62ff225036ea4ef3506b697009ea0ca51
NavaneethaRk <30334357+NavaneethaRk@users.noreply.github.com>
2017-08-13 15:36:03 +0530

    leap.c

---
+++ leap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int year;
+    scanf("%d",&year);
+    if(year%4==0)
+    {
+        printf("Leap year");
+    }
+    else
+    {
+        printf("Non leap year");
+    }
+    return 0;
+}
