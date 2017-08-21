86b502c1255d32c733afa83baea119e76ff12fb7
NavaneethaRk <30334357+NavaneethaRk@users.noreply.github.com>
2017-08-21 14:20:50 +0530

    oddno.c

---
+++ oddno.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+int main()
+{
+    int i,n,g;
+    scanf("%d %d",&n,&g);
+    for(i=n;i<=g;i++)
+    {
+        if(i%2!=0)
+        {
+            printf("%d ",i);
+        }
+    }
+    return 0;
+}
